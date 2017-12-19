8a15b9c038800937871e6dcc76a2af8e6e09a491
Antoine Sauray <sauray.antoine@gmail.com>
2017-12-19 18:35:04 +0100

    FIX pdf full view

--- Parade/ViewControllers/PdfViewController.swift
+++ Parade/ViewControllers/PdfViewController.swift
@@ -7,24 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class PdfViewController: UIViewController {
     
     var productPdf: ProductPdf?
+    var webView: WKWebView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.orientationChanged),
+            name: .UIDeviceOrientationDidChange,
+            object: nil)
         //Bundle.main.url(forResource: pv.videoName, withExtension: pv.videoType)!
         if let pdfURL = Bundle.main.url(forResource: productPdf?.pdfName, withExtension: "pdf")  {
             do {
                 print("do")
                 let data = try Data(contentsOf: pdfURL)
                 print("data")
-                let webView = UIWebView(frame: CGRect(x:20,y:20,width:view.frame.size.width-40, height:view.frame.size.height-40))
+                webView = WKWebView(frame: CGRect(x: 0,y: 0,width:view.frame.size.width, height:view.frame.size.height))
                 print("webview")
-                webView.load(data, mimeType: "application/pdf", textEncodingName:"", baseURL: pdfURL.deletingLastPathComponent())
-                view.addSubview(webView)
+                webView?.load(data, mimeType: "application/pdf", characterEncodingName:"", baseURL: pdfURL.deletingLastPathComponent())
+                view.addSubview(webView!)
                 
             }
             catch {
@@ -36,4 +43,13 @@ class PdfViewController: UIViewController {
             print("pdf not found")
         }
     }
+    
+    @objc func orientationChanged(notification: NSNotification) {
+        print("orientation changed")
+        if let wv = webView {
+            wv.frame = CGRect(x: 0,y: 0,width:view.frame.size.width, height:view.frame.size.height)
+        }
+        
+    }
+    
 }
